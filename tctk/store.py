1296c16d949c17835d126115e041e589c39ef74d
Patrick Branch <branch.patrick@gmail.com>
2026-02-06 00:50:11 -0500

    load/save raffle data

--- tctk/store.py
+++ tctk/store.py
@@ -1,15 +1,26 @@
 from typing import Any, ClassVar, Self, Type
 
+import json
 import polars as pl
 from polars import DataFrame
 from dataclasses import dataclass
 import dataclasses
+import pathlib
 
+def get_data_file_path(cls):
+    return pathlib.Path(f"{cls.__name__}.parquet")
+
+def write(cls):
+    cls.store.write_parquet(get_data_file_path(cls))
+
+def read(cls):
+    cls.store.read_parquet(get_data_file_path(cls))
 
 def save[T](cls: Type[T], me: T):
     row = dataclasses.asdict(me)
     new = pl.DataFrame([row])
     cls.store = pl.concat([cls.store, new])
+    write(cls)
 
 @dataclass
 class UserRaffle:
@@ -18,12 +29,17 @@ class UserRaffle:
     did_win: bool
     join_time: int
 
-    store: ClassVar[DataFrame] = DataFrame(schema={
-        "username": pl.String,
-        "raffle_start_time": pl.UInt32,
-        "did_win": pl.Boolean,
-        "join_time": pl.UInt32
-    })
+    @classmethod
+    def load(cls):
+        if get_data_file_path(cls).exists():
+            read(cls)
+        else:
+            cls.store: DataFrame = DataFrame(schema={
+                "username": pl.String,
+                "raffle_start_time": pl.UInt32,
+                "did_win": pl.Boolean,
+                "join_time": pl.UInt32
+            })
 
     def save(self):
         save(UserRaffle, self)
@@ -34,11 +50,16 @@ class Raffle:
     duration: int
     amount: int
 
-    store: ClassVar[DataFrame] = DataFrame(schema={
-        "start_time": pl.UInt32,
-        "duration": pl.UInt8,
-        "amount": pl.UInt16
-    })
+    @classmethod
+    def load(cls):
+        if get_data_file_path(cls).exists():
+            read(cls)
+        else:
+            cls.store: DataFrame = DataFrame(schema={
+                "start_time": pl.UInt32,
+                "duration": pl.UInt8,
+                "amount": pl.UInt16
+            })
 
     def save(self):
         save(Raffle, self)
